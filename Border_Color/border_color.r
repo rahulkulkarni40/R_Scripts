eab9f3e2fefe32f539a7cf8fe23eb54633e361e9
Rahul Kulkarni <rahul.kulkarni1994@gmail.com>
2020-03-16 17:08:05 +0530

    Add files via upload
    Updated code

--- Border_Color/border_color.r
+++ Border_Color/border_color.r
@@ -1,23 +1,60 @@
+#importing Library that is been istalled
 library(shiny)
 library(shinyjs)
 
+#for creating or adding UI for the R Application
 ui<-shinyUI(
   fluidPage(
+    
+    #Title of the page
+    titlePanel("Border Color Example"),
     useShinyjs(),
-    selectInput("TagColor", "Color of my Tag",choices=c("Red","Blue",
+    
+    #Side Bar Layout
+    sidebarLayout(
+      
+      # SIde Bar Panel
+      sidebarPanel(
+      
+        # Select Iput is like combobox in HTML
+        selectInput("TagColor", "Color of my Tag",choices=c("Red","Blue",
                                                         "Yellow","Black", "grey","orange","pink","purple"), 
-                selected = "Red", multiple = FALSE),
-    verbatimTextOutput("Pop",placeholder = TRUE)
-  )
+                selected = "Red", multiple = FALSE)),
+    #Main Panel
+    mainPanel(
+      
+      #Paragraph Data
+      p("This Application cahnges the Border Color of the Text Output."),
+      
+      #Bold Texts
+      strong("On Change of Options in Select Input."),
+      
+      #new Line character
+      br(),
+      br(),
+    
+      #It is an Output box of R shiny Item
+    div(style="width:300px;padding-left:10px;",fluidRow(verbatimTextOutput("Pop",placeholder = TRUE)))
+  )))
 )
 
 server<-shinyServer(function(input, output) {
+  # As soon as the event or data is occured
   observe({
+    
+    #Storing the input value to the variable 
     d <- input$TagColor
+    #storing color value
     color <- d
-      color <- paste0("solid ", tolower(color))
-      runjs(paste0("document.getElementById('Pop').style.border ='",color,"'"))
-      output$Pop <-renderText({d})
+    
+    #Converting the string to lower case
+    color <- paste0("solid ", tolower(color))
+      
+    #Setting the value on the basis of Id using Java script Package of R  
+    runjs(paste0("document.getElementById('Pop').style.border ='",color,"'"))
+      
+    # dISPLAYING THE TEXT VALUE to the text out put in the UI
+    output$Pop <-renderText({d})
   })
 })
 
